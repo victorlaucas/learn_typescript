15ff35218272dea9272cb7b0222d873ded6f771f
Victor <victorlaucas@gmail.com>
2017-12-20 14:05:10 -0700

    Variables in ts

---
+++ 006_variables.js
@@ -0,0 +1,15 @@
+// three types of variables 
+// var fullName : string = "Victor Laucas";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Kierra Orvin";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
+}
+printName("Victor", "Laucas");
+//# sourceMappingURL=006_variables.js.map
---
+++ 006_variables.ts
@@ -0,0 +1,22 @@
+// three types of variables 
+
+var fullName : string = "Victor Laucas";
+let paidAccount : boolean = true;
+const versionNumber : number = 1.3;
+
+fullName = "Kierra Orvin";
+paidAccount = false;
+
+console.log(fullName);
+console.log(paidAccount);
+console.log(versionNumber);
+
+// function printName(f, l) {
+//   var greeting : string = "Hi there, ";
+//   console.log(greeting + f + " " + l);
+
+//   var greeting : string = "Hi there, ";
+//   console.log(greeting + f + " " + l);
+// }
+
+// printName("Victor", "Laucas");
