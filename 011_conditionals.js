988a9f7e3bfa12f1bbc742bb19bead7faa90aec3
Victor <victorlaucas@gmail.com>
2017-12-21 19:05:01 -0700

    Basic if else statement in ts

---
+++ 011_conditionals.js
@@ -0,0 +1,11 @@
+var password = "zxczxc";
+if (password == "asdfasdf") {
+    console.log("Yes, this is the password");
+}
+else if (password == "zxczxc") {
+    console.log("Yes, this is another password");
+}
+else {
+    console.log("Wrong Password");
+}
+//# sourceMappingURL=011_conditionals.js.map
---
+++ 011_conditionals.ts
@@ -0,0 +1,9 @@
+let password : string = "zxczxc";
+
+if (password == "asdfasdf") {
+  console.log("Yes, this is the password");
+} else if (password == "zxczxc") {
+  console.log("Yes, this is another password")
+} else {
+  console.log("Wrong Password");
+}
