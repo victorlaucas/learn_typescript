f4420114dec99e6064360e2282885f3d86215018
Victor <victorlaucas@gmail.com>
2017-12-21 19:15:21 -0700

    TS conditional operators

---
+++ 012_conditional_operators.js
@@ -0,0 +1,8 @@
+var x = 100;
+if (x == 200) {
+    console.log('Conditional passed');
+}
+if (x === 100) {
+    console.log('Conditional passed');
+}
+//# sourceMappingURL=012_conditional_operators.js.map
---
+++ 012_conditional_operators.ts
@@ -0,0 +1,32 @@
+let x : number = 100;
+
+if(x==200){
+  console.log('Conditional passed');
+}
+
+// == only checks for equivalent value, ignores data type 
+// === Checks not only for value, but verifies that the data type is the same
+
+if(x===100){
+  console.log('Conditional passed');
+}
+
+if(x != 100){
+  console.log('Conditional passed');
+}
+
+if(x > 100) {
+  console.log('Condition passed');
+}
+
+if(x >= 100) {
+  console.log('Condition passed');
+}
+
+if(x < 100) {
+  console.log('Condition passed');
+}
+
+if(x <= 100) {
+  console.log('Condition passed');
+}
