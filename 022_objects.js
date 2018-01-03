89605ceeec1b24446938b4a71626aa1c4e35b41c
Victor <victorlaucas@gmail.com>
2018-01-02 23:29:04 -0700

    How to create and call objects in ts

---
+++ 022_objects.js
@@ -0,0 +1,13 @@
+var realUser = {
+    email: 'test@test.com',
+    firstName: 'Victor',
+    lastName: 'Laucas',
+    sayHi: function () {
+        return "Hey there!";
+    }
+};
+console.log(realUser.email);
+console.log(realUser.firstName);
+console.log(realUser.lastName);
+console.log(realUser.sayHi());
+//# sourceMappingURL=022_objects.js.map
