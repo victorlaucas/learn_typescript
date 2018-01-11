b85540cb7fc3f83ff22e7689a37c7be8017623f5
Victor <victorlaucas@gmail.com>
2018-01-10 18:37:44 -0700

    Method Decorators

---
+++ 032_method_decorator.ts
@@ -0,0 +1,26 @@
+@detailedLog('billing')
+class AccountsPayable {
+  constructor() {}
+
+  @admin
+  deleteAccount() {
+    console.log('Deleting account...');
+  }
+}
+
+function detailedLog(dashboard: string) {
+  if (dashboard == 'billing') {
+    console.log("Working in the billing department");
+    return function (target: Object) {};
+  } else {
+    return function (target: Object) {};
+  }
+}
+
+function admin(target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) : any {
+  console.log("Doing admin check");
+  return descriptor;;
+}
+
+var post = new AccountsPayable;
+post.deleteAccount();
